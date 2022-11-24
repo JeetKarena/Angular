4598a40b080972b2dd74bed98be300e1ce0f132f
Jeet Karena <jeetkarena3@gmail.com>
2022-11-24 11:36:08 +0530

    Home And Page Not Found Component Added & Routing

--- ApiApp/src/app/app.module.ts
+++ ApiApp/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
